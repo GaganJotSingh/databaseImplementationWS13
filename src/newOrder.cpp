479b6f11d27355baa3c74b975e229604d6d60b0f
GaganJotSingh <singhgagan.tum.informatics@gmail.com>
2013-10-24 02:14:06 +0200

    For calling loader functions.

---
+++ src/newOrder.cpp
@@ -0,0 +1,20 @@
+#include "tableLoader.cpp"
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+//  load_warehouse();
+//  load_district();
+//  load_customer();
+//  load_history();
+//  load_neworder();
+//  load_order();
+//  load_orderline();
+//  load_item();
+//  load_stock();
+  
+  return 0;
+}
