10af162707a1d5ed3df6f8391b0d401876e363e4
GaganJotSingh <singhgagan.tum.informatics@gmail.com>
2013-11-03 03:42:03 +0100

    Parsing from sql schema to cpp structs completed.

--- src/task2/parser/Schema.cpp
+++ src/task2/parser/Schema.cpp
@@ -25,49 +25,35 @@ static std::string type(const Schema::Relation::Attribute& attr) {
       case Types::Tag::Timestamp:
          return "Timestamp";
    }
-   
-/*   switch(type) {
-      case Types::Tag::Integer:
-         return "Integer";
-      case Types::Tag::Numeric: {
-         std::stringstream ss;
-         ss << "Numeric(" << attr.len1 << ", " << attr.len2 << ")";
-         return ss.str();
-      }
-      case Types::Tag::Char: {
-         std::stringstream ss;
-         ss << "Char(" << attr.len << ")";
-         return ss.str();
-      }
-      case Types::Tag::Varchar: {
-         std::stringstream ss;
-         ss << "Varchar(" << attr.len << ")";
-         return ss.str();
-      }
-      case Types::Tag::Timestamp:
-         return "Timestamp";
-   }*/
    throw;
 }
 
 std::string Schema::toString() const {
    std::stringstream out;
+   out <<"#include \"Types.hpp\"\n#include <vector>\n#include <map>\n#include <tuple>\n\nusing namespace std;\n\n";
    for (const Schema::Relation& rel : relations) {
       out << "struct " << rel.name << " {" << std::endl;
       for (const auto& attr : rel.attributes)
          out << '\t' << type(attr) << " " << attr.name << ";" << std::endl;
-      out << "};\n\n";
+      out << "};\n";
+      if(rel.primaryKey.size() > 0) {
+         out << "std::map<";
+         if(rel.primaryKey.size() == 1) {
+            out << type(rel.attributes[rel.primaryKey.back()]);
+         } else {
+            out << "tuple<";
+            for (unsigned pKeyId : rel.primaryKey) {
+               out << type(rel.attributes[pKeyId]);
+               if(pKeyId != rel.primaryKey.back()) {
+                  out << ", ";
+               }
+            }
+            out << ">";
+         }
+         out << ", uint64_t> " << "map_" + rel.name << ";\n";
+      }
+      out << "vector<" << rel.name << "> " << "t_" + rel.name << ";\n\n";
    }
-   
-/*   for (const Schema::Relation& rel : relations) {
-      out << rel.name << std::endl;
-      out << "\tPrimary Key:";
-      for (unsigned keyId : rel.primaryKey)
-         out << ' ' << rel.attributes[keyId].name;
-      out << std::endl;
-      for (const auto& attr : rel.attributes)
-         out << '\t' << attr.name << '\t' << type(attr) << (attr.notNull ? " not null" : "") << std::endl;
-   }*/
-   
+         
    return out.str();
 }
