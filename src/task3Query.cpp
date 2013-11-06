5d86097cd0d659036a03dc4cb5140beb8a01dddc
GaganJotSingh <singhgagan.tum.informatics@gmail.com>
2013-11-06 10:26:41 +0100

    Using hash join for query.

--- src/task3Query.cpp
+++ src/task3Query.cpp
@@ -1,55 +1,66 @@
 #include <cstdint>
-#include "Types.hpp"
+//#include "Types.hpp"
 
 #include "tableLoader.cpp"
-#include <iostream>
-#include <cstdio>
+//#include <iostream>
+//#include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <unordered_map>
 
 using namespace std;
 
 void joinQuery() {
 std::cout<<"Inside joinQuery\n";
-  vector<uint64_t> temp_cust;
-  vector<uint64_t> temp_ord;
-  vector<uint64_t> temp_ol;
-  
+  std::unordered_map<tuple<Integer, Integer, Integer>, uint64_t> uMapCust;
+  std::unordered_map<tuple<Integer, Integer, Integer>, uint64_t> uMapOrder;
+	
+  struct customer cust;
   for(uint64_t x1=0; x1 < t_customer.size(); x1++) {
-      if(t_customer[x1].c_last.value[0] == 'B') {
-          temp_cust.push_back(x1);
+      cust = t_customer[x1];
+      if(cust.c_last.value[0] == 'B') {	      
+          //temp_cust.push_back(x1);
+		  uMapCust.insert(std::pair<tuple<Integer, Integer, Integer>, uint64_t>(std::tuple<Integer, Integer, Integer>(cust.c_w_id, cust.c_d_id, cust.c_id), x1));
       }
   }
-
-cout<<"x1 done\n";
-
+cout<<"Loop x1 done\n";
+		  
+  struct order odr;
   for(uint64_t x2 = 0; x2 < t_order.size(); x2++) {
-      for(uint64_t y2 = 0; y2 < temp_cust.size(); y2++) {
+      odr = t_order[x2];
+	  if(uMapCust.count(std::tuple<Integer, Integer, Integer>(odr.o_w_id, odr.o_d_id, odr.o_c_id)) == 1) {
+	      uMapOrder.insert(std::pair<tuple<Integer, Integer, Integer>, uint64_t>(std::tuple<Integer, Integer, Integer>(odr.o_w_id, odr.o_d_id, odr.o_id), x2));
+	  }
+/*      for(uint64_t y2 = 0; y2 < temp_cust.size(); y2++) {
           if(t_customer[temp_cust[y2]].c_id == t_order[x2].o_c_id) {
               temp_ord.push_back(x2);
           }
-      }
+      }*/
   }
+cout<<"Loop x2 done\n";
 
-cout<<"One loop x2 y2 done\n";
-  
+  uint64_t sum = 0;  
+  struct orderline ol;
   for(uint64_t x3 = 0; x3 < t_orderline.size(); x3++) {
-      for (uint64_t y3 = 0; y3 < temp_ord.size(); y3++) {
+      ol = t_orderline[x3];
+      if(uMapOrder.count(std::tuple<Integer, Integer, Integer>(ol.ol_w_id, ol.ol_d_id, ol.ol_o_id)) == 1) {
+	      sum = sum + ((uint64_t)ol.ol_quantity.value * (uint64_t)ol.ol_amount.value);
+	  }
+/*      for (uint64_t y3 = 0; y3 < temp_ord.size(); y3++) {
           if((t_order[temp_ord[y3]].o_w_id == t_orderline[x3].ol_w_id) && (t_order[temp_ord[y3]].o_d_id == t_orderline[x3].ol_d_id) && (t_order[temp_ord[y3]].o_id == t_orderline[x3].ol_o_id)){
               temp_ol.push_back(x3);
           }
-      }
+      }*/
   }
+  cout<<"Result of query is: Sum = "<<sum<<endl;
+cout<<"Loop x3 done\n";
 
-cout<<"One loop x3 y3 done\n";
-  
-  uint64_t sum = 0;
+/*
   for(uint64_t x4 = 0; x4 < temp_ol.size(); x4++) {
       sum = sum + (uint64_t)t_orderline[temp_ol[x4]].ol_quantity.value * (uint64_t)t_orderline[temp_ol[x4]].ol_amount.value;
   }
+*/
 
-cout<<"Result of query is: Sum = "<<sum<<endl;
-  
   return;
 }
 
